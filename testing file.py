01a5694cc6c5b359cc73d5fa6114d9dd68ffce8f
Adeeb339 <a.a.adeeb.2013@gmail.com>
2026-02-14 13:21:46 +0600

    added testing method for substraction

--- testing file.py
+++ testing file.py
@@ -9,14 +9,17 @@ class test_cal(unittest.TestCase):
         self.assertEqual(Aresult2, 1)
         Aresult3=cal.add(-1,-2)
         self.assertEqual(Aresult3, -3)
-        
+
     def test_sub(self):
-        Bresult1=cal.sub(2,1)
-        self.assertEqual(Bresult1, 1)
-        Bresult2=cal.sub(-2,1)
-        self.assertEqual(Bresult2, -3)
-        Bresult3=cal.sub(-2,-1)
-        self.assertEqual(Bresult3, -1) 
+        Aresult1=cal.sub(2,1)
+        self.assertEqual(Aresult1, 1)
+        Aresult2=cal.sub(-2,1)
+        self.assertEqual(Aresult2, -3)
+        Aresult3=cal.sub(-2,-1)
+        self.assertEqual(Aresult3, -1)
 
 if __name__ == "__main__":
-    unittest.main()                                                             
+    unittest.main()
+    
+
+                                                                
