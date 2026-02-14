61bbbb4da835e2c4fc698885e2055e15b26c4325
Adeeb339 <a.a.adeeb.2013@gmail.com>
2026-02-14 13:37:33 +0600

    added testing method for multiplication and division

--- testing file.py
+++ testing file.py
@@ -3,20 +3,36 @@ import cal
 
 class test_cal(unittest.TestCase):
     def test_add(self):
-        Aresult1=cal.add(1,2)
-        self.assertEqual(Aresult1, 3)
-        Aresult2=cal.add(-1,2)
-        self.assertEqual(Aresult2, 1)
-        Aresult3=cal.add(-1,-2)
-        self.assertEqual(Aresult3, -3)
+        result1=cal.add(1,2)
+        self.assertEqual(result1, 3)
+        result2=cal.add(-1,2)
+        self.assertEqual(result2, 1)
+        result3=cal.add(-1,-2)
+        self.assertEqual(result3, -3)
 
     def test_sub(self):
-        Aresult1=cal.sub(2,1)
-        self.assertEqual(Aresult1, 1)
-        Aresult2=cal.sub(-2,1)
-        self.assertEqual(Aresult2, -3)
-        Aresult3=cal.sub(-2,-1)
-        self.assertEqual(Aresult3, -1)
+        result1=cal.sub(2,1)
+        self.assertEqual(result1, 1)
+        result2=cal.sub(-2,1)
+        self.assertEqual(result2, -3)
+        result3=cal.sub(-2,-1)
+        self.assertEqual(result3, -1)
+
+    def test_mult(self):
+        result1=cal.mult(2,1)
+        self.assertEqual(result1, 2)
+        result2=cal.mult(-2,1)
+        self.assertEqual(result2, -2)
+        result3=cal.mult(-2,-1)
+        self.assertEqual(result3, 2)
+    
+    def test_div(self):
+        result1=cal.div(2,2)
+        self.assertEqual(result1, 1)
+        result2=cal.div(-2,2)
+        self.assertEqual(result2, -1)
+        result3=cal.div(-2,-2)
+        self.assertEqual(result3, 1)
 
 if __name__ == "__main__":
     unittest.main()
